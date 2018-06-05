a7bdbe019610b2d7eeb8eb9ac744cb17aa1e0542
saurabh-vijay004 <saurabh.vijay@iksulaops.com>
2018-06-05 11:31:21 +0530

    pods installed

--- Damas POC/ViewController.swift
+++ Damas POC/ViewController.swift
@@ -8,11 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
+    @IBOutlet weak var usenameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if UserDefaults.standard.string(forKey: "username")! != nil {
+            
+            usenameTextField.text = UserDefaults.standard.string(forKey: "username")!
+            
+            passwordTextField.text = UserDefaults.standard.string(forKey: "password")!
+            
+        } else{
+            
+            print(UserDefaults.standard.string(forKey: "username")!)
+        
+        }
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +37,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func submit(_ sender: Any) {
+        
+        if (usenameTextField.text?.isEmpty)! {
+            
+            print("Enter UserName")
+            
+        } else if (passwordTextField.text?.isEmpty)!{
+         
+            print("Enter UserName")
+            
+        } else{
+            
+            UserDefaults.standard.setValue(usenameTextField.text, forKey: "username")
+            UserDefaults.standard.setValue(passwordTextField.text, forKey: "password")
+            
+        }
+        
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
+    }
+    
 
 }
 
---
+++ Podfile
@@ -0,0 +1,17 @@
+platform :ios, '10.0'
+ 
+inhibit_all_warnings!
+use_frameworks!
+ 
+target 'Damas POC' do
+  pod 'Alamofire', '~> 4.2.0'
+  pod 'AccordionMenuSwift', '~> 1.2.6'
+  pod 'NVActivityIndicatorView'
+  pod 'DLRadioButton', '~> 1.4'
+  pod 'DropDown'
+  pod 'Cosmos', '~> 10.0'
+  pod 'SDWebImage/WebP'
+  pod 'ImageSlideshow', '~> 1.3'
+  pod 'ImageSlideshow/Alamofire'
+end
+
